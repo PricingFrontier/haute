e7a13447630f1851737931250429cfa714dcd82f
Ralph <ralph@pricing-frontier.co.uk>
2026-07-25 12:34:37 +0100

    Retire completed supply-chain roadmap item

--- tests/test_docs_accuracy.py
+++ tests/test_docs_accuracy.py
@@ -251,6 +251,7 @@ def test_component_roadmaps_are_flat_complete_and_self_contained() -> None:
         "AUD-C11",
         "AUD-C13",
         "AUD-C18",
+        "AUD-SEC-01",
         "AUD-SEC-02",
         "AUD-TRACE-01",
         "ROAD-EXEC-01",
